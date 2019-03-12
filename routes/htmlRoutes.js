6cb57103389aed192a345f401c9cf8947ec44e63
unknown <bumprnickel@gmail.com>
2019-03-11 21:27:24 -0500

    added user template

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,16 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.render("index");
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExamples) {
       res.render("example", {
-        example: dbExample
+        example: dbExamples
       });
     });
   });
