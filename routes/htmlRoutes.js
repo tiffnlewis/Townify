ffc298967451eca26ec51594fb9f21baa54a5e5a
unknown <bumprnickel@gmail.com>
2019-03-13 20:25:16 -0500

    added basic user profile

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/example", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
